7b8ada99df5fcd516768b4acc3429e1300756935
AltheaSajuela-UCM <honeiryuu@gmail.com>
2025-09-17 21:56:58 +0800

    Added Chapter_1 exercises

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+
+
+public class FavoriteSong {
+   public static void main (String[] args) {
+   
+      System.out.println("I keep it hundred in case you twist your blade");
+      System.out.println("I cut you out because I don't think that I'm sick");
+      System.out.println("Be still your heart, and do not resuscitate");
+      System.out.println("In the end, you took it all away from me");
+      
+      //So Far So Fake - Pierce The Veil :3
+       }
+}
